b7803ae000687599f38742f0bc7b27c3b265a5b2
Rohan Anand <thededlier@gmail.com>
2020-03-24 12:36:29 +0000

    remove warnings

--- src/ie/tcd/lucene/scobo/parsers/ContentParser.java
+++ src/ie/tcd/lucene/scobo/parsers/ContentParser.java
@@ -1,8 +1,6 @@
 package ie.tcd.lucene.scobo.parsers;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
